1cf0bc19ab3fe90394a918955b98057ebd6aa13c
yangangchen <yangangchen.0@gmail.com>
2017-10-13 16:23:33 -0400

    Final version!

--- gitupload.sh
+++ gitupload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git init
-git remote add origin https://github.com/yangangchen/Sample_Python_Scripts.git
+#git init
+#git remote add origin https://github.com/yangangchen/Sample_Python_Scripts.git
 git remote show origin
 git add .
 git commit -m 'Final version!'
