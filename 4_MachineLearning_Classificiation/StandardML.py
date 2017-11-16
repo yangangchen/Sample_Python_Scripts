f69049f54934d99acd2cd90b89d4f8ef249e766d
yangangchen <yangangchen.0@gmail.com>
2017-11-16 14:59:01 -0500

    Final version!

--- 4_MachineLearning_Classificiation/StandardML.py
+++ 4_MachineLearning_Classificiation/StandardML.py
@@ -1,3 +1,28 @@
+# StandardML.py
+# 
+# Copyright (C) 2017  Yangang Chen
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# 
+# 
+# 
+# Standard machine learning classifiers (e.g. Support Vector Classifier, 
+# Gradient Boosting Classifier, etc) for the preprocessed data "train.csv" and "test.csv"
+
+################################
+
 import pandas as pd
 from sklearn.svm import SVC
 from sklearn.ensemble import GradientBoostingClassifier
