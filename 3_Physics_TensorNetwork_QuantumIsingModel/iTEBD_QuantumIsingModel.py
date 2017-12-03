3a9b2b41156dee23e79259d956706441ee26a2aa
yangangchen <yangangchen.0@gmail.com>
2017-12-03 13:41:24 -0500

    Final version!

---
+++ 3_Physics_TensorNetwork_QuantumIsingModel/iTEBD_QuantumIsingModel.py
@@ -0,0 +1,209 @@
+# iTEBD_QuantumIsingModel.py
+#
+# Author: Yangang Chen
+#
+# Copyright (C) 2017  Yangang Chen
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Use iTEBD algorithm to compute the ground state energy and magnatization
+of the 1D infinite Quantum Ising Model. See
+
+http://uni10-tutorials.readthedocs.io/en/latest/lecture1.html
+
+for a description. The code is only implemented by numpy and scipy library."""
+
+import numpy as np
+from scipy.linalg import expm
+import matplotlib.pyplot as plt
+
+np.random.seed(1)
+
+
+def hamiltonian(h, spin_dim=2):
+    sx = np.array([[0, 1], [1, 0]])
+    sz = np.array([[1, 0], [0, -1]])
+    si = np.identity(spin_dim)
+    H = np.tensordot(sz, sz, axes=0) + h * 1 / 2 * (
+        np.tensordot(sx, si, axes=0) + np.tensordot(si, sx, axes=0))
+    H = np.transpose(H, (0, 2, 1, 3))
+    return H
+
+
+def magnatization(direction='x', spin_dim=2):
+    sx = np.array([[0, 1], [1, 0]])
+    sz = np.array([[1, 0], [0, -1]])
+    si = np.identity(spin_dim)
+    if direction == 'x':
+        M = 1 / 2 * (
+            np.tensordot(sx, si, axes=0) + np.tensordot(si, sx, axes=0))
+    elif direction == 'z':
+        M = 1 / 2 * (
+            np.tensordot(sz, si, axes=0) + np.tensordot(si, sz, axes=0))
+    M = np.transpose(M, (0, 2, 1, 3))
+    return M
+
+
+def unitary(dt, H, spin_dim=2):
+    return expm(-dt * H.reshape((spin_dim ** 2, spin_dim ** 2)))
+
+
+class TensorNetwork:
+    def __init__(self, bond_dim, spin_dim=2):
+        self.bond_dim = bond_dim
+        self.spin_dim = spin_dim
+
+        self.GammaA = self._initialize_Gamma()
+        self.GammaB = self._initialize_Gamma()
+        self.LambdaA = self._initialize_Lambda()
+        self.LambdaB = self._initialize_Lambda()
+
+    def _initialize_Gamma(self):
+        GammaA = np.random.randn(self.bond_dim, self.bond_dim, self.spin_dim)
+        # GammaA, _ = np.linalg.qr(np.random.randn(self.bond_dim * self.spin_dim, self.bond_dim))
+        # GammaA = GammaA.reshape((self.bond_dim, self.spin_dim, self.bond_dim))
+        # GammaA = np.transpose(GammaA, (0, 2, 1))
+        return GammaA
+
+    def _initialize_Lambda(self):
+        LambdaA = np.random.randn(self.bond_dim)
+        LambdaA *= 1 / np.linalg.norm(LambdaA)
+        return LambdaA
+
+    def _contract_AB(self, GammaA, GammaB, LambdaA, LambdaB):
+        A = np.tensordot(GammaA, np.diag(LambdaA), axes=(1, 0))
+        A = np.transpose(A, (0, 2, 1))
+        B = np.tensordot(GammaB, np.diag(LambdaB), axes=(1, 0))
+        B = np.transpose(B, (0, 2, 1))
+        AB = np.tensordot(A, B, axes=(1, 0))
+        AB = np.transpose(AB, (0, 2, 1, 3))
+        return AB
+
+    def _apply_unitary(self, AB, LambdaB, U):
+        AB1 = np.tensordot(np.diag(LambdaB), AB, axes=(1, 0))
+        AB1_mat = AB1.reshape((self.bond_dim ** 2, self.spin_dim ** 2))
+        Theta = AB1_mat.dot(U)
+        Theta = Theta.reshape((self.bond_dim, self.bond_dim, self.spin_dim, self.spin_dim))
+        return Theta
+
+    def _tensor_svd(self, Theta):
+        Theta1 = np.transpose(Theta, (0, 2, 1, 3))
+        Theta1_mat = Theta1.reshape((self.bond_dim * self.spin_dim, self.bond_dim * self.spin_dim))
+        X, LambdaA, Y = np.linalg.svd(Theta1_mat)
+        X = X[:, :self.bond_dim]
+        X = X.reshape((self.bond_dim, self.spin_dim, self.bond_dim))
+        X = np.transpose(X, (0, 2, 1))
+        Y = Y[:self.bond_dim, :]
+        Y = Y.reshape((self.bond_dim, self.bond_dim, self.spin_dim))
+        LambdaA = LambdaA[:self.bond_dim]
+        LambdaA *= 1 / np.linalg.norm(LambdaA)
+        return X, Y, LambdaA
+
+    def _renormalize_Gamma(self, X, Y, LambdaB):
+        GammaA = np.tensordot(np.diag(1 / LambdaB), X, axes=(1, 0))
+        GammaB = np.tensordot(Y, np.diag(1 / LambdaB), axes=(1, 0))
+        GammaB = np.transpose(GammaB, (0, 2, 1))
+        return GammaA, GammaB
+
+    def _left_right_eigs(self, AB):
+        flag = 1
+        ABAB = np.tensordot(AB, AB, axes=((2, 3), (2, 3)))
+        ABAB = np.transpose(ABAB, (0, 2, 1, 3))
+        ABAB_mat = ABAB.reshape((self.bond_dim ** 2, self.bond_dim ** 2))
+        D1, U1 = np.linalg.eig(ABAB_mat)
+        Right = U1[:, 0]
+        D2, U2 = np.linalg.eig(ABAB_mat.transpose())
+        Left = U2[:, 0]
+        if abs(D1[0] - 1) > 1e-3 or abs(D2[0] - 1) > 1e-3:
+            print('Mistake: the largest eigenvalue must be equal to 1, but it is ' + str([D1[0], D2[0]]))
+            flag = 0
+        norm = Left.dot(Right)
+        Left = Left.reshape((self.bond_dim, self.bond_dim))
+        Right = Right.reshape((self.bond_dim, self.bond_dim))
+        return Left, Right, norm, flag
+
+    def _iTEBD_onestep(self, GammaA, GammaB, LambdaA, LambdaB, U):
+        AB = self._contract_AB(GammaA, GammaB, LambdaA, LambdaB)
+        Theta = self._apply_unitary(AB, LambdaB, U)
+        X, Y, LambdaA = self._tensor_svd(Theta)
+        GammaA, GammaB = self._renormalize_Gamma(X, Y, LambdaB)
+        return GammaA, GammaB, LambdaA, LambdaB
+
+    def iTEBD(self, U, N):
+        for n in range(N + 1):
+            self.GammaA, self.GammaB, self.LambdaA, self.LambdaB = \
+                self._iTEBD_onestep(self.GammaA, self.GammaB, self.LambdaA, self.LambdaB, U)
+            self.GammaB, self.GammaA, self.LambdaB, self.LambdaA = \
+                self._iTEBD_onestep(self.GammaB, self.GammaA, self.LambdaB, self.LambdaA, U)
+
+    def _operator_expectation(self, op, GammaA, GammaB, LambdaA, LambdaB):
+        AB = self._contract_AB(GammaA, GammaB, LambdaA, LambdaB)
+        Left, Right, norm, flag = self._left_right_eigs(AB)
+        Left = Left.ravel()
+        Right = Right.ravel()
+        Tensor = np.tensordot(AB, op, axes=((2, 3), (0, 1)))
+        Tensor = np.tensordot(Tensor, AB, axes=((2, 3), (2, 3)))
+        Tensor = np.transpose(Tensor, (0, 2, 1, 3))
+        Matrix = Tensor.reshape((self.bond_dim ** 2, self.bond_dim ** 2))
+        value = Left.dot(Matrix.dot(Right))
+        value /= norm
+        return value, flag
+
+    def operator_expectation(self, op):
+        value1, flag1 = self._operator_expectation(op, self.GammaA, self.GammaB, self.LambdaA, self.LambdaB)
+        value2, flag2 = self._operator_expectation(op, self.GammaB, self.GammaA, self.LambdaB, self.LambdaA)
+        value = 1 / 2 * (value1 + value2)
+        flag = flag1 * flag2
+        return value, flag
+
+
+def main():
+    dt = 0.02
+    N = 1000
+    bond_dim = 10
+    h_array = np.linspace(0, 3, 31)
+    energy_array = np.zeros(len(h_array))
+    magx_array = np.zeros(len(h_array))
+    for i in range(len(h_array)):
+        print(i)
+        h = h_array[i]
+        H = hamiltonian(h)
+        U = unitary(dt, H)
+        Mx = magnatization(direction='x')
+        while 1:
+            TN = TensorNetwork(bond_dim=bond_dim)
+            TN.iTEBD(U, N)
+            energy_array[i], flag1 = TN.operator_expectation(H)
+            magx_array[i], flag2 = TN.operator_expectation(Mx)
+
+            if flag1 * flag2:
+                break
+
+    result = np.vstack([h_array, energy_array, magx_array])
+
+    plt.figure()
+    plt.plot(h_array, energy_array, 'o-')
+    plt.show()
+
+    plt.figure()
+    plt.plot(h_array, magx_array, 'o-')
+    plt.show()
+
+    np.save('result.npy', result)
+
+
+####################################################
+
+if __name__ == '__main__':
+    main()
